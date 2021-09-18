7b071d5e389a0295d9a5dc4ffecb4c63a3640347
Kacper Ksykiewicz <kacperksykiewicz@MacBook-Pro-Kacper.local>
2021-09-18 18:34:44 +0200

    7.2 niemutowalny użytkownik forum

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 public class StreamMain {
 
@@ -14,5 +15,8 @@ public class StreamMain {
         poemBeautifier.beautify("Z tej przyczyny zrodziliśmy się bez wprawy i pomrzemy bez rutyny.", String::toUpperCase);
         poemBeautifier.beautify("Choćbyśmy uczniami byli najtępszymi w szkole świata,", String::toLowerCase);
         poemBeautifier.beautify("nie będziemy repetować żadnej zimy ani lata.", textToBeautify -> "_****_" + textToBeautify + "_####_");
+
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
    }
 }
