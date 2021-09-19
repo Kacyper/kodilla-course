b58fb6645eaff0832e2a3faba6ebf226bde0da0e
Kacper Ksykiewicz <kacperksykiewicz@MacBook-Pro-Kacper.local>
2021-09-19 14:27:30 +0200

    Forum User

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,22 +1,18 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.iterate.NumbersGenerator;
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
+import java.util.stream.Collectors;
 
 public class StreamMain {
 
-    public static void main(final String[] args) {
+    public static void main(String[] args) {
+        BookDirectory theBookDirectory = new BookDirectory();
+        String theResultStringOfBooks = theBookDirectory.getList().stream()  // [1]
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .map(Book::toString)
+                .collect(Collectors.joining(",\n","<<",">>"));                    // [2]
 
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
-
-        System.out.println("Nic dwa razy - W.Szymborska\n");
-
-        poemBeautifier.beautify("Nic dwa razy się nie zdarza i nie zdarzy.", (textToBeautify -> "ABC_" + textToBeautify + "_ABC\n"));
-        poemBeautifier.beautify("Z tej przyczyny zrodziliśmy się bez wprawy i pomrzemy bez rutyny.", String::toUpperCase);
-        poemBeautifier.beautify("Choćbyśmy uczniami byli najtępszymi w szkole świata,", String::toLowerCase);
-        poemBeautifier.beautify("nie będziemy repetować żadnej zimy ani lata.", textToBeautify -> "_****_" + textToBeautify + "_####_");
-
-        System.out.println("Using Stream to generate even numbers from 1 to 20");
-        NumbersGenerator.generateEven(20);
-   }
+        System.out.println(theResultStringOfBooks);
+    }
 }
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/Beautifier.java
@@ -0,0 +1,16 @@
+package com.kodilla.stream.beautifier;
+
+public class Beautifier {
+
+    public static void main(final String[] args) {
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+
+        System.out.println("Nic dwa razy - W.Szymborska\n");
+
+        poemBeautifier.beautify("Nic dwa razy się nie zdarza i nie zdarzy.", (textToBeautify -> "ABC_" + textToBeautify + "_ABC\n"));
+        poemBeautifier.beautify("Z tej przyczyny zrodziliśmy się bez wprawy i pomrzemy bez rutyny.", String::toUpperCase);
+        poemBeautifier.beautify("Choćbyśmy uczniami byli najtępszymi w szkole świata,", String::toLowerCase);
+        poemBeautifier.beautify("nie będziemy repetować żadnej zimy ani lata.", textToBeautify -> "_****_" + textToBeautify + "_####_");
+    }
+}
