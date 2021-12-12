06785459790233de2ab7121e01c580ba8b2bd04c
Kacper <kksykiewicz@gmail.com>
2021-12-12 18:02:08 +0100

    17.3 naglowki faktur i ich pozycje

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
@@ -1,8 +1,12 @@
 package com.kodilla.hibernate.tasklist;
 
 
+import com.kodilla.hibernate.task.Task;
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "TASKLISTS")
@@ -11,6 +15,7 @@ class TaskList {
     private int id;
     private String listName;
     private String description;
+    private List<Task> tasks = new ArrayList<>();
 
 
     public TaskList() {
@@ -37,6 +42,16 @@ class TaskList {
         return listName;
     }
 
+    @OneToMany(
+            targetEntity = Task.class,
+            mappedBy = "taskList",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
     public void setListName(String listName) {
         this.listName = listName;
     }
@@ -49,5 +64,7 @@ class TaskList {
         this.description = description;
     }
 
-
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
 }
---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -0,0 +1,71 @@
+package com.kodilla.hibernate.invoice.dao;
+
+import com.kodilla.hibernate.invoice.Invoice;
+import com.kodilla.hibernate.invoice.Item;
+import com.kodilla.hibernate.invoice.Product;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.math.BigDecimal;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+@SpringBootTest
+public class InvoiceDaoTestSuite {
+
+    @Autowired
+    private ProductDao productDao;
+    @Autowired
+    private ItemDao itemDao;
+    @Autowired
+    private InvoiceDao invoiceDao;
+
+    @Test
+    void testInvoiceDaoSave(){
+        //Given
+        Product product1 = new Product("Carrot");
+        Product product2 = new Product("Apple");
+        Product product3 = new Product("iPhone");
+        Product product4 = new Product("Water");
+
+        productDao.save(product1);
+        productDao.save(product2);
+        productDao.save(product3);
+        productDao.save(product4);
+
+        Invoice invoice1 = new Invoice("13213123");
+        Invoice invoice2 = new Invoice("21312323");
+
+        invoiceDao.save(invoice1);
+        invoiceDao.save(invoice2);
+
+        Item item1 = new Item(product1, new BigDecimal(10), 2, invoice1);
+        Item item2 = new Item(product2, new BigDecimal(20), 1, invoice1);
+        Item item3 = new Item(product3, new BigDecimal(3000), 1, invoice2);
+        Item item4 = new Item (product4, new BigDecimal(10), 3, invoice1);
+
+        itemDao.save(item1);
+        itemDao.save(item2);
+        itemDao.save(item3);
+        itemDao.save(item4);
+
+        //When
+        int invoiceOne = invoice1.getId();
+        int invoiceTwo = invoice2.getId();
+
+
+        //Then
+        assertNotEquals(0, invoiceOne);
+        assertNotEquals(0L, invoiceTwo);
+
+        //CleanUp
+        try {
+            invoiceDao.deleteById(invoiceOne);
+            invoiceDao.deleteById(invoiceTwo);
+        } catch (Exception e) {
+            //do nothing
+        }
+
+    }
+}
