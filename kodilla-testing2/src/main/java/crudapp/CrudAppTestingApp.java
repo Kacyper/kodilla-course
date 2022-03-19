431401c512408158cd2d154d46f40654df3b8aff
Kacper <kksykiewicz@gmail.com>
2022-03-19 19:16:15 +0100

    31.2 zakladanie konta na FB

--- kodilla-testing2/src/main/java/crudapp/CrudAppTestingApp.java
+++ kodilla-testing2/src/main/java/crudapp/CrudAppTestingApp.java
@@ -1,2 +1,19 @@
-package crudapp;public class CrudAppTestingApp {
+package crudapp;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class CrudAppTestingApp {
+    public static void main(String[] args) {
+        WebDriver driver = new SafariDriver();
+        driver.get("https://kacyper.github.io");
+
+        WebElement searchField = driver.findElement(By.xpath("//html/body/main/section/form/fieldset/input"));
+        searchField.sendKeys("New robotic task");
+
+        WebElement textArea = driver.findElement(By.xpath("//html/body/main/section[1]/form/fieldset[2]/textarea"));
+        textArea.sendKeys("The robotic content");
+    }
 }
--- kodilla-testing2/src/main/java/facebook/FacebookTestingApp.java
+++ kodilla-testing2/src/main/java/facebook/FacebookTestingApp.java
@@ -1,2 +1,36 @@
-package facebook;public class FacebookTestingApp {
+package facebook;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.support.ui.Select;
+
+public class FacebookTestingApp {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = new SafariDriver();
+        driver.get("https://www.facebook.com/");
+
+        WebElement cookies = driver.findElement(By.xpath("//div[contains(@class, \"_9xo5\")]/button[2]"));
+        cookies.click();
+
+        WebElement registrationForm = driver.findElement(By.xpath("//div[contains(@class, \"_6ltg\")]/a[1]"));
+        registrationForm.click();
+
+
+        Thread.sleep(2000);
+
+        WebElement birthDay = driver.findElement(By.xpath("//select[@name=\"birthday_day\"]"));
+        Select selectDay = new Select(birthDay);
+        selectDay.selectByIndex(2);
+
+        WebElement birthMonth = driver.findElement(By.xpath("//select[@name=\"birthday_month\"]"));
+        Select selectMonth = new Select(birthMonth);
+        selectMonth.selectByIndex(11);
+
+        WebElement birthYear = driver.findElement(By.xpath("//select[@name=\"birthday_year\\\"]"));
+        Select selectYear = new Select(birthYear);
+        selectYear.selectByIndex(12);
+
+    }
 }
