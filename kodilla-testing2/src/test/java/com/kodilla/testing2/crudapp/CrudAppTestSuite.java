fa46d29baa6ab56c438c3c398d82028b3943f3bd
Kacper <kksykiewicz@gmail.com>
2022-03-20 18:28:37 +0100

    31.3 - sprzatnie w aplikacji crud app

--- kodilla-testing2/src/test/java/com/kodilla/testing2/crudapp/CrudAppTestSuite.java
+++ kodilla-testing2/src/test/java/com/kodilla/testing2/crudapp/CrudAppTestSuite.java
@@ -33,17 +33,17 @@ public class CrudAppTestSuite {
         final String XPATH_TASK_NAME = "//form[contains(@action,\"createTask\")]/fieldset[1]/input";
         final String XPATH_TASK_CONTENT = "//form[contains(@action,\"createTask\")]/fieldset[2]/textarea";
         final String XPATH_ADD_BUTTON = "//form[contains(@action,\"createTask\")]/fieldset[3]/button";
-        String taskName = "Task number " + generator.nextInt(100000);            // [1]
-        String taskContent = taskName + " content";                              // [2]
+        String taskName = "Task number " + generator.nextInt(100000);
+        String taskContent = taskName + " content";
 
         WebElement name = driver.findElement(By.xpath(XPATH_TASK_NAME));
-        name.sendKeys(taskName);                                                 // [3]
+        name.sendKeys(taskName);
 
         WebElement content = driver.findElement(By.xpath(XPATH_TASK_CONTENT));
-        content.sendKeys(taskContent);                                           // [4]
+        content.sendKeys(taskContent);
 
-        WebElement addButton = driver.findElement(By.xpath(XPATH_ADD_BUTTON));   // [5]
-        addButton.click();                                                       // [6]
+        WebElement addButton = driver.findElement(By.xpath(XPATH_ADD_BUTTON));
+        addButton.click();
         Thread.sleep(2000);
 
         return taskName;
